48c3b1284df62dc3e05da1a971220538e7c42e40
Владимир <chivladimir@yandex.ru>
2024-10-28 19:46:01 +0300

    AGer search engine. Start

--- .venv/SandBox.py
+++ .venv/SandBox.py
@@ -6,6 +6,7 @@ html = """
 <h1>Test</h1>
 <ol>
 	<li>First item</li>
+	<li>Item with empty link <a >link</a></li>
 	<li>Item with <a href="http://hse.ru">link</a></li>
 	<li>Another item with <a href="http://wikipedia.org">another link</a>.</li>
 </ol>
@@ -22,17 +23,22 @@ srch_2 = soup.find("ol")
 print('srch_2',srch_2)
 
 srch_3 = soup.find("a")
-print('srch_3', srch_3)
-print('srch_3+', srch_3["href"])  # ask content with tag <href> from <a>
+print(srch_3)
+#print(srch_3['href']) # ask content with tag <href> from <a>
 
-srch_4 = soup.find_all("li")  # return list[]
-print('srch_4', len(srch_4), srch_4)
-print('srch_4+', srch_4[2])
+srch_4 = soup.find_all("li") # return list[]
+print(len(srch_4), srch_4)
+print(srch_4[2])
 
-# Task - return all links from <a>
-_links = soup.find_all("a")
-print(_links)
-i = 0
-for i in range(len(_links)):
-    print(_links[i]["href"])
-    i += 1
+#Task - return all links from <a>
+
+# for a in soup.find_all("a"):
+# 	print(a['href'])
+
+for a in soup.find("ol").find_all("a"):
+	print(a.get('href'))
+
+for a in soup.find("ol").find_all("a"):
+	href = a.get('href')
+	if href is not None:
+		print(href)
