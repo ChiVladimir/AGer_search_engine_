7aad53be58a4dd54e7e9489f733d8ebee6026ec2
Vladimir Chichetkin <chivladimir@yandex.ru>
2025-02-21 16:05:06 +0300

    Wikidict search engine. Database procedures

--- .venv/database.py
+++ .venv/database.py
@@ -1,16 +1,16 @@
 import sqlite3
 
 
-connection = sqlite3.connectionect('roots&words.db')
+connection = sqlite3.Connection('roots&words.db')
 cursor = connection.cursor()
-cursorsor.execute('''
+cursor.execute('''
 CREATE TABLE IF NOT EXISTS Roots(
 id_root INTEGER,
-id_roots_group TEXT PRIMARY KEY,
+roots_group TEXT PRIMARY KEY,
 root TEXT NOT NULL
 )
 ''')
-cursorsor.execute('''
+cursor.execute('''
 CREATE TABLE IF NOT EXISTS Words(
 id INTEGER PRIMARY KEY,
 word TEXT NOT NULL,
@@ -21,13 +21,13 @@ id_root TEXT
 def filling_roots(args):
     s = list
     roots_list = list(args.split(' '))
-    id_ = cursor.execute("SELECT id_root_group FROM Roots;").fetchall()
-    new_id_root_group = max(id_) + 1
-    print (new_id_root_group)
+    id_ = cursor.execute("SELECT id_root FROM Roots;").fetchall()
+    new_id_root = max(id_) + 1
+    print (new_id_root)
     for i in range(len(roots_list)):
-        id_root = str(new_id_root_group)|"R"|str(i + 1)
+        id_root_group = args
         root = roots_list[i]
-        cursor.execute(f'INSERT INTO Roots (id_root_group, id_root, root) VALUES({new_id_root_group}, {id_root}, {root});')
+        cursor.execute(f'INSERT INTO Roots (id_root_group, id_root, root) VALUES({root_group}, {id_root}, {root});')
     connection.commit()
 
 def filling_words(args):
@@ -37,6 +37,7 @@ def filling_words(args):
     for i in range(len(word_list)):
         check_word = cursor.execute(f"SELECT COUNT(*) FROM Words WHERE word = {word_list[i]};")
         if check_word > 0:
+            print ((f"Word |{word_list[i]}| already exist. Check please manualy or update root special procedure."))
             continue
         else:
             id = id_ + i
@@ -44,6 +45,14 @@ def filling_words(args):
             cursor.execute(f'INSERT INTO Words (id, word) VALUES({id}, {word});')
     connection.commit()
 
+def update_word_and_root(word, root):
+    check_word = cursor.execute(f"SELECT COUNT(*) FROM Words WHERE word = {word} and id_root not Null;").fetchall()
+    if int(check_word) > 0:
+        print ((f"Root for the word |{word}| already exist. Check please manualy"))
+    else:
+        id_root = id_roots_group
+        cursor.execute(f'UPDATE Words SET id_root = {id_root} WHERE id = {id_word};')
+    connection.commit()
 
 def word_connect_root(word, root, id_root):
     id_word = cursor.execute(f"SELECT id FROM Words WHERE word = {word};")
@@ -51,30 +60,34 @@ def word_connect_root(word, root, id_root):
     cursor.execute(f'UPDATE Words SET id_root = {id_root} WHERE id = {id_word}')
     connection.commit()
 
-def get_all():
-    s = cursor.execute("SELECT * FROM users;").fetchall()
-    connection.commit()
-    return s
+# def get_all():
+#     s = cursor.execute("SELECT * FROM users;").fetchall()
+#     connection.commit()
+#     return s
+#
+# def count():
+#     s = cursor.execute("SELECT COUNT(*) FROM users;").fetchone()
+#     connection.commit()
+#     return s[0]
+#
+# def get_id():
+#     s = cursor.execute("SELECT id FROM users;").fetchall()
+#     connection.commit()
+#     return s
+#
+# def check_block(id):
+#     s = cursor.execute("SELECT * FROM block; ").fetchall()
+#     connection.commit()
+#     return (id,) in s
+#
+# def block(id):
+#     cursor.execute(f"INSERT INTO block VALUES({id}); ").fetchall()
+#     connection.commit()
 
-def count():
-    s = cursor.execute("SELECT COUNT(*) FROM users;").fetchone()
-    connection.commit()
-    return s[0]
-
-def get_id():
-    s = cursor.execute("SELECT id FROM users;").fetchall()
-    connection.commit()
-    return s
-
-def check_block(id):
-    s = cursor.execute("SELECT * FROM block; ").fetchall()
-    connection.commit()
-    return (id,) in s
-
-def block(id):
-    cursor.execute(f"INSERT INTO block VALUES({id}); ").fetchall()
+def delete_root(id):
+    cursor.execute(f"DELETE FROM Root WHERE id_root = {id}; ").fetchall()
     connection.commit()
 
-def delete(id):
-    cursor.execute(f"DELETE FROM block WHERE id = {id}; ").fetchall()
+def delete_word(id):
+    cursor.execute(f"DELETE FROM Word WHERE id = {id}; ").fetchall()
     connection.commit()
