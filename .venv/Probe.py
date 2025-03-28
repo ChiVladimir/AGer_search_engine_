ecd9789178c27b0aa1c7edf7836f6cbd1c921cbd
Vladimir Chichetkin <chivladimir@yandex.ru>
2025-03-28 22:12:10 +0300

    ZZap price-list loader

--- .venv/Probe.py
+++ .venv/Probe.py
@@ -1,15 +1,57 @@
-def root_cleaning(root):
-    hook_1 = root.find("/")
-    hook_2 = root.find("&")
+from sys import argv
+import webbrowser
+import base64
+import requests
+import config
+import time
 
-    return f'@{root[(hook_1 + 1):hook_2]}$'
 
-'''
-То есть вместо
-да/да1/даж&дать
-должно быть 
-@да1/даж$
+module, filename = argv
 
-'''
+data = open(filename, 'rb').read()
 
-print (root_cleaning("да/да1/даж&дать"))
+base64_encoded = base64.b64encode(data).decode('UTF-8')
+
+login = config.USERNAME
+password = config.PASSWD
+code_templ = 350016735	#int код шаблона (этот шаблон должен быть включен и иметь тип 'Загрузка прайса через API')
+url = ""	#да	ссылка на прайс-лист
+file_body =	base64_encoded#string содержимое прайс-листа в кодировке base64
+file_name = filename#	string имя файла
+api_key = config.API
+
+print (login)
+print (password)
+print (code_templ)
+print (url)
+print (file_body)
+print (file_name)
+print (api_key)
+
+response_1 = requests.get('https://api.github.com')
+print(response_1.status_code)
+
+
+response_2 = requests.post(f'https://api.zzap.pro/webservice/datasharing.asmx/UploadTemplatePrice?login={login}'
+                          f'&password={password}&code_templ={code_templ}&url={url}&file_body={file_body}'
+                          f'&file_name={file_name}&api_key={api_key}')
+
+print(f'https://api.zzap.pro/webservice/datasharing.asmx/UploadTemplatePrice?login={login}&password={password}'
+        f'&code_templ={code_templ}&url={url}&file_body={file_body}&file_name={filename}&api_key={api_key}')
+print(response_2.status_code)
+time.sleep(10)
+
+BASE_URL = "https://api.zzap.pro/webservice/datasharing.asmx/UploadTemplatePrice"
+data_to_post = {"login":login,
+                "password":password,
+                "code_templ":code_templ,
+                "url":url,
+                "file_body":file_body,
+                "file_name":file_name,
+                "api_key":api_key}
+
+print(data_to_post)
+#exit()
+response_3 = requests.post(f"{BASE_URL}/post", json = data_to_post)
+print(response_3.status_code)
+time.sleep(10)
--- .venv/launcher.py
+++ .venv/launcher.py
@@ -1,3 +1,5 @@
 import os
 
-os.system('python zzap_price_list_sender.py output-onlinetools.txt')
+#os.system('python zzap_price_list_sender.py zzap_test_4_api_.csv')
+
+os.system('python Probe.py zzap_test_4_api_.csv')
