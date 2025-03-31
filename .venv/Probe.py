45932d522d42b85e801f269b13678da7f74366c0
Vladimir Chichetkin <chivladimir@yandex.ru>
2025-03-31 10:06:47 +0300

    ZZap price-list loader

--- .venv/Probe.py
+++ .venv/Probe.py
@@ -4,6 +4,7 @@ import base64
 import requests
 import config
 import time
+import json
 
 
 module, filename = argv
@@ -15,31 +16,18 @@ base64_encoded = base64.b64encode(data).decode('UTF-8')
 login = config.USERNAME
 password = config.PASSWD
 code_templ = 350016735	#int код шаблона (этот шаблон должен быть включен и иметь тип 'Загрузка прайса через API')
-url = ""	#да	ссылка на прайс-лист
-file_body =	base64_encoded#string содержимое прайс-листа в кодировке base64
+url = ""	#ссылка на прайс-лист - не нужна для прямого обращения через API, содержащим file_body base64_encoded
+file_body = base64_encoded #string содержимое прайс-листа в кодировке base64
 file_name = filename#	string имя файла
 api_key = config.API
-
-print (login)
-print (password)
-print (code_templ)
-print (url)
-print (file_body)
-print (file_name)
-print (api_key)
-
-response_1 = requests.get('https://api.github.com')
-print(response_1.status_code)
-
-
-response_2 = requests.post(f'https://api.zzap.pro/webservice/datasharing.asmx/UploadTemplatePrice?login={login}'
-                          f'&password={password}&code_templ={code_templ}&url={url}&file_body={file_body}'
-                          f'&file_name={file_name}&api_key={api_key}')
-
-print(f'https://api.zzap.pro/webservice/datasharing.asmx/UploadTemplatePrice?login={login}&password={password}'
-        f'&code_templ={code_templ}&url={url}&file_body={file_body}&file_name={filename}&api_key={api_key}')
-print(response_2.status_code)
-time.sleep(10)
+#
+# print (login)
+# print (password)
+# print (code_templ)
+# print (url)
+# print (file_body)
+# print (file_name)
+# print (api_key)
 
 BASE_URL = "https://api.zzap.pro/webservice/datasharing.asmx/UploadTemplatePrice"
 data_to_post = {"login":login,
@@ -50,8 +38,7 @@ data_to_post = {"login":login,
                 "file_name":file_name,
                 "api_key":api_key}
 
-print(data_to_post)
-#exit()
-response_3 = requests.post(f"{BASE_URL}/post", json = data_to_post)
-print(response_3.status_code)
-time.sleep(10)
+
+response_3 = requests.post(BASE_URL, json = data_to_post)
+#print(response_3.status_code)
+#time.sleep(10)
--- .venv/zzap_price_list_sender.py
+++ .venv/zzap_price_list_sender.py
@@ -1,13 +1,11 @@
-#-*- coding: UTF-8 -*-
-
 from sys import argv
 import webbrowser
 import base64
 import requests
 import config
-import asyncio
+import time
+import json
 
-BASE_URL = "https://api.zzap.pro/webservice/datasharing.asmx/UploadTemplatePrice"
 
 module, filename = argv
 
@@ -18,11 +16,20 @@ base64_encoded = base64.b64encode(data).decode('UTF-8')
 login = config.USERNAME
 password = config.PASSWD
 code_templ = 350016735	#int код шаблона (этот шаблон должен быть включен и иметь тип 'Загрузка прайса через API')
-url = ""  # Пусто для отправки данных в file_body
-file_body =	base64_encoded #string содержимое прайс-листа в кодировке base64
-file_name = filename
+url = ""	#ссылка на прайс-лист - не нужна для прямого обращения через API, содержащим file_body base64_encoded
+file_body = base64_encoded #string содержимое прайс-листа в кодировке base64
+file_name = filename#	string имя файла
 api_key = config.API
+#
+# print (login)
+# print (password)
+# print (code_templ)
+# print (url)
+# print (file_body)
+# print (file_name)
+# print (api_key)
 
+BASE_URL = "https://api.zzap.pro/webservice/datasharing.asmx/UploadTemplatePrice"
 data_to_post = {"login":login,
                 "password":password,
                 "code_templ":code_templ,
@@ -31,8 +38,7 @@ data_to_post = {"login":login,
                 "file_name":file_name,
                 "api_key":api_key}
 
-print(data_to_post)
-#exit()
-response = requests.post(f"{BASE_URL}/post", json = data_to_post)
 
-#print(response.status_code, response.json())
+response_3 = requests.post(BASE_URL, json = data_to_post)
+#print(response_3.status_code)
+#time.sleep(10)
